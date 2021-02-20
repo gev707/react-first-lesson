3b12ca7acd9fe797c7480416faa45cb9ffd9e7a4
gev707 <gev.mar92@gmail.com>
2021-02-20 17:09:13 +0400

    Cars

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-
-
+import Car from './Components/Car'
 function App() {
   return (
     <div className="App">
-      
+      <h1>React Lesson</h1>
+      <Car />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Components/Car.js
@@ -0,0 +1,9 @@
+
+export default  () => (
+    <div className='carHolder'>
+        <h2>This is Car Component</h2>
+        <p>Car Name: <strong>{'BMW'}</strong></p>
+        <p>Car Color: <strong>{'Black'}</strong></p>
+        <p>Car Year: <strong>{2010}</strong></p>
+    </div>
+);
